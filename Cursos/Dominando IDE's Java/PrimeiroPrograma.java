752795a925d4fbddc4aade9e80cd08b315d7c2d9
lari987 <michelazzolarissa272@gmail.com>
2022-07-16 15:38:50 -0300

    update

---
+++ Cursos/Dominando IDE's Java/PrimeiroPrograma.java
@@ -0,0 +1,8 @@
+public class PrimeiroPrograma {
+       public static void main(String args[]) {
+         int a = 2;
+         int b = 3;
+         int soma = a + b;
+         System.out.println(soma);
+    }
+}
