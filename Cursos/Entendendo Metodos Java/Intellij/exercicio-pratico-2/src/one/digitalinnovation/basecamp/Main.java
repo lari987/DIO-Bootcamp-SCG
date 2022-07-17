5fac9af285040b7be14b7a4bb2ccba99c2ec5ee4
lari987 <michelazzolarissa272@gmail.com>
2022-07-16 22:51:31 -0300

    adicionado o segundo exercicio pratico de metodos

---
+++ Cursos/Entendendo Metodos Java/Intellij/exercicio-pratico-2/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Exercicio Quadrilatero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(5f,5f);
+        Quadrilatero.area(7,8,9);
+    }
+}
