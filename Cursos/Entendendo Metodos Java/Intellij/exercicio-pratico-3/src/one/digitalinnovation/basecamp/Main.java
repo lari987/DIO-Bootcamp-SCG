d9864e9865df7a3aa4cf4bad8a57f7d6c1990d2c
lari987 <michelazzolarissa272@gmail.com>
2022-07-20 14:41:54 -0300

    adicionando o ultimo exercicio pratico do curso de metodos

---
+++ Cursos/Entendendo Metodos Java/Intellij/exercicio-pratico-3/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+public static void main(String[] args){
+    System.out.println("Area do quadrado: " + Quadrilatero.area(3));
+    System.out.println("Area do retangulo: " + Quadrilatero.area(5,5));
+    System.out.println("Area do trapezio: " + Quadrilatero.area(7,8,9));
+}
+}
